346e2ec9fcd574e5988ba3e13607933326ddb068
Joaaalves <contato@joao-alves.com>
2025-11-04 15:30:02 -0300

    chore: added custom settings for eslint

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,18 +1,22 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import globals from "globals"
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+	...nextVitals,
+	...nextTs,
+	// Override default ignores of eslint-config-next.
+	globalIgnores([
+		// Default ignores of eslint-config-next:
+		".next/**",
+		"out/**",
+		"build/**",
+		"next-env.d.ts",
+	]),
+	{ ignores: [".next/**", "public/**", "next.config.js", "postcss.config.js", "node_modules/**"] },
+	{ files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
 ]);
 
 export default eslintConfig;

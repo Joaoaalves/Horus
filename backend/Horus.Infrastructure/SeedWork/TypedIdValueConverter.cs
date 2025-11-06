4b8518022cae44bde251ffa4e608cd3fb2940ddb
Joaaalves <contato@joao-alves.com>
2025-11-05 21:31:52 -0300

    feat: add EF Package feat: add Strongly Typed ID converter for EF

---
+++ backend/Horus.Infrastructure/SeedWork/TypedIdValueConverter.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Horus.Domain.SeedWork;
+
+namespace Horus.Infrastructure.SeedWork
+{
+    /// <summary>
+    /// Converts a strongly typed ID (e.g., TypedIdValueBase) to and from a Guid for EF Core.
+    /// </summary>
+    /// <typeparam name="TTypedIdValue">The strongly typed ID class.</typeparam>
+    public class TypedIdValueConverter<TTypedIdValue>(ConverterMappingHints? mappingHints = null)
+        : ValueConverter<TTypedIdValue, Guid>(
+            id => id.Value,
+            value => Create(value),
+            mappingHints)
+        where TTypedIdValue : TypedIdValueBase
+    {
+        private static TTypedIdValue Create(Guid id)
+        {
+            return (Activator.CreateInstance(typeof(TTypedIdValue), id) as TTypedIdValue)!;
+        }
+    }
+}

b0e9a8c0fb91473da537a2c471e53db4e8193288
Joaaalves <contato@joao-alves.com>
2025-11-05 21:26:52 -0300

    feat: add DomainNotification base

---
+++ backend/Horus.Application/Configuration/DomainEvents/IDomainEventNotification.cs
@@ -0,0 +1,14 @@
+using Horus.Domain.SeedWork;
+
+namespace Horus.Application.Configuration.DomainEvents
+{
+    public interface IDomainEventNotification<TEventType> : IDomainEventNotification
+    {
+        TEventType DomainEvent { get; }
+    }
+
+    public interface IDomainEventNotification : INotification
+    {
+        Guid Id { get; }
+    }
+}

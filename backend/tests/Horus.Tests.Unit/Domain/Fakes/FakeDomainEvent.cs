a02c895d239b8adaaf7adc37f38f0f35561ed8f4
Joaaalves <contato@joao-alves.com>
2025-11-05 21:23:36 -0300

    feat: add SeedWork tests for Domain structures

---
+++ backend/tests/Horus.Tests.Unit/Domain/Fakes/FakeDomainEvent.cs
@@ -0,0 +1,9 @@
+using Horus.Domain.SeedWork;
+
+namespace Horus.Tests.Unit.Domain.Fakes
+{
+    public class FakeDomainEvent : IDomainEvent
+    {
+        public DateTime OccurredOn => new();
+    }
+}
---
+++ backend/tests/Horus.Tests.Unit/Domain/Fakes/FakeTypedId.cs
@@ -0,0 +1,8 @@
+using Horus.Domain.SeedWork;
+
+namespace Horus.Tests.Unit.Domain.Fakes
+{
+    public class FakeTypedId(Guid value) : TypedIdValueBase(value)
+    {
+    }
+}
